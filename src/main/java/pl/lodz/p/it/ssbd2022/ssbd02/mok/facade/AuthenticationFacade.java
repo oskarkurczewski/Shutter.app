102761fc978ff9d295bcca3c7e0b5f7b17011c69
Michał <michal.piotrowski.317@gmail.com>
2022-04-29 20:54:25 +0200

    Dodane wyjtatki, findByLogin rzuca wyjatek

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/mok/facade/AuthenticationFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/mok/facade/AuthenticationFacade.java
@@ -1,12 +1,15 @@
 package pl.lodz.p.it.ssbd2022.ssbd02.mok.facade;
 
 import pl.lodz.p.it.ssbd2022.ssbd02.entity.User;
+import pl.lodz.p.it.ssbd2022.ssbd02.exceptions.CustomApplicationException;
+import pl.lodz.p.it.ssbd2022.ssbd02.exceptions.NoAuthenticatedUser;
 import pl.lodz.p.it.ssbd2022.ssbd02.util.FacadeTemplate;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
@@ -25,9 +28,13 @@ public class AuthenticationFacade extends FacadeTemplate<User> {
         return em;
     }
 
-    public User findByLogin(String login) {
+    public User findByLogin(String login) throws NoAuthenticatedUser {
         TypedQuery<User> query = getEm().createNamedQuery("user.findByLogin", User.class);
         query.setParameter("login", login);
-        return query.getSingleResult();
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            throw CustomApplicationException.NoAuthenticatedUser();
+        }
     }
 }
