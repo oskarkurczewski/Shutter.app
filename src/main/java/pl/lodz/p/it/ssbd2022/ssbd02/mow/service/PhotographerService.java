e76e1003de938bbb9fdb870f18cd305ba4dbb434
Dominik Łapiński <dominiklapinski.pl@gmail.com>
2022-05-17 23:38:35 +0200

    Adds Photographer Service and Endpoint

---
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/mow/service/PhotographerService.java
@@ -0,0 +1,24 @@
+package pl.lodz.p.it.ssbd2022.ssbd02.mow.service;
+
+import pl.lodz.p.it.ssbd2022.ssbd02.entity.PhotographerInfo;
+import pl.lodz.p.it.ssbd2022.ssbd02.exceptions.*;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+
+import static pl.lodz.p.it.ssbd2022.ssbd02.security.Roles.*;
+
+@Stateless
+public class PhotographerService {
+
+    @PermitAll
+    public PhotographerInfo findPhotographerInfo(String login) throws NoPhotographerFoundException {
+        throw new UnsupportedOperationException();
+    }
+
+    @RolesAllowed(changePhotographerDescription)
+    public void changeDescription(PhotographerInfo photographer, String newDescription) {
+        throw new UnsupportedOperationException();
+    }
+}
