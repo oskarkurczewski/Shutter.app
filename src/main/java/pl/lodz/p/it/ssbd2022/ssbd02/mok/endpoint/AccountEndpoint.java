dcb71455f007b6dfd19ef48c01164bb47b847d4f
Dominik Łapiński <dominiklapinski.pl@gmail.com>
2022-05-18 22:38:14 +0200

    Poprawiono błędne role w @RolesAllowed dla getAccountInfo

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/mok/endpoint/AccountEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/mok/endpoint/AccountEndpoint.java
@@ -170,7 +170,7 @@ public class AccountEndpoint {
      * @throws NoAuthenticatedAccountFound W przypadku gdy dane próbuje uzyskać niezalogowana osoba
      * @see AccountInfoDto
      */
-    @RolesAllowed({CLIENT, PHOTOGRAPHER})
+    @RolesAllowed({ADMINISTRATOR, PHOTOGRAPHER, CLIENT, PHOTOGRAPHER})
     public AccountInfoDto getOwnAccountInfo() throws NoAuthenticatedAccountFound {
         Account account = authenticationContext.getCurrentUsersAccount();
         return new AccountInfoDto(account);
