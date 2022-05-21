d2d63caf0d5c13752eb4f3c76b9d4e8c3fb7f1ab
mdzieciuchowicz <229874@edu.p.lodz.pl>
2022-05-21 20:11:12 +0200

    Dodano komponent okna oraz poprawiono style

---
+++ src/main/frontend/src/components/shared/Modal/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./style.scss";
+import Button from "../Button";
+import Card from "../Card";
+import ModalRoot from "./ModalRoot";
+
+type Props =
+   | {
+        className?: string;
+        type?: "confirm";
+        isOpen: boolean;
+        onCancel: () => void;
+        onSubmit: () => void;
+        title: string;
+     }
+   | {
+        className?: string;
+        type?: "info";
+        isOpen: boolean;
+        onCancel?: never;
+        onSubmit: () => void;
+        title: string;
+     };
+
+const Modal: React.FC<Props> = ({
+   className,
+   children,
+   type = "info",
+   isOpen,
+   onCancel,
+   onSubmit,
+   title,
+}) => {
+   if (!isOpen) {
+      return null;
+   }
+
+   return (
+      <ModalRoot>
+         <div className="modal-backdrop">
+            <Card className={`modal-wrapper ${className ? className : ""}`}>
+               {title && (
+                  <div className="modal-header">
+                     <p className="category-title">{title}</p>
+                  </div>
+               )}
+               <div className="modal-content">{children}</div>
+               <div className="modal-footer">
+                  {type === "confirm" && (
+                     <Button className="cancel" onClick={() => onCancel && onCancel()}>
+                        Cancel
+                     </Button>
+                  )}
+                  <Button className="submit" onClick={() => onSubmit()}>
+                     Submit
+                  </Button>
+               </div>
+            </Card>
+         </div>
+      </ModalRoot>
+   );
+};
+
+export default Modal;
