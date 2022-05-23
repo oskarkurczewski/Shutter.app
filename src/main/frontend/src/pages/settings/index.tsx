d18285c1989127881a92d31ebdf49fe7d74da6d1
Oskar Kurczewski <229935@edu.p.lodz.pl>
2022-05-23 16:25:59 +0200

    dodano widok stawania się fotografem i zaprzestania byciem fotografem (ukrywanie informacji o fotografie)

--- src/main/frontend/src/pages/settings/index.tsx
+++ src/main/frontend/src/pages/settings/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
 import Card from "components/shared/Card";
+import Button from "components/shared/Button";
 import { HiCamera } from "react-icons/hi";
 import { IoSettingsSharp } from "react-icons/io5";
 import { useLocation } from "react-router-dom";
@@ -15,8 +16,13 @@ const SettingsPage: React.FC = () => {
       },
       {
          icon: <HiCamera />,
-         id: "photographer",
+         id: "photographer-become",
          label: "Ustawienia fotografa",
+      },
+      {
+         icon: <HiCamera />,
+         id: "photographer-profile",
+         label: "Ustawienia profilu fotografa",
       },
    ];
 
@@ -47,6 +53,48 @@ const SettingsPage: React.FC = () => {
                      laboriosam consequatur rerum nulla temporibus consequuntur, veritatis
                      distinctio numquam sed fugiat doloribus modi aut ullam.
                   </p>
+               </div>
+            </Card>
+            <Card id="photographer-become">
+               <p className="category-title">Ustawienia Fotografa</p>
+               <div className="content">
+                  <p>
+                     W każdym momencie możesz zostać fotografem! Dzięki temu zyskasz
+                     możliwość tworzenia profilu, umieszczania w nim zdjęć, a także
+                     zarządzania rezerwacjami.
+                  </p>
+                  <div className="buttons">
+                     <Button
+                        onClick={() => {
+                           console.log("todo");
+                        }}
+                     >
+                        Zostań fotografem
+                     </Button>
+                  </div>
+               </div>
+            </Card>
+            <Card id="photographer-profile">
+               <p className="category-title">Ustawienia Profilu Fotografa</p>
+               <div className="content">
+                  <p>
+                     W każdej chwili możesz edytować widoczność swojego profilu fotografa.
+                  </p>
+                  <p>
+                     Uwaga! Po dokonaniu tej operacji zostaną anulowane wszystkie Twoje
+                     przyszłe rezerwacje, a klienci dostaną wiadomość o wyłączeniu
+                     widoczności profilu fotografa.
+                  </p>
+                  <div className="buttons">
+                     <Button
+                        className="red-button"
+                        onClick={() => {
+                           console.log("todo");
+                        }}
+                     >
+                        Wyłącz widoczność profilu
+                     </Button>
+                  </div>
                </div>
             </Card>
          </div>
