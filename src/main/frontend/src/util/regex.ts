d236da8f73dce916458d955ebd4c2d6e026d5b0c
Filip Grzechnik 229888 <229888@edu.p.lodz.pl>
2022-06-14 00:27:19 +0200

    usunięto komentarze

--- src/main/frontend/src/util/regex.ts
+++ src/main/frontend/src/util/regex.ts
@@ -5,7 +5,6 @@ export const nameSurnamePattern = new RegExp("^[A-Za-zĄĆĘŃŚŁÓŻŹąćęń
 export const emailPattern = new RegExp(
    "^(?=.{1,64}@)[A-Za-z0-9_-]+(.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$"
 );
-// TODO: fix password regex
 export const passwordPattern = new RegExp(
    "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()\\-{}:;',?/*~$^+=<>-]).{8,64}$"
 );
