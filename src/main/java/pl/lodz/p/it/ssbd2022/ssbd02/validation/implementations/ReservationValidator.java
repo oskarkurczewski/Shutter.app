9343b6e28dedb4a66f41bccde2d5bb9dad6984bf
229983 Michal Piotrowski <229983@edu.p.lodz.pl>
2022-06-15 15:15:30 +0200

    Poprawa reservationValidator

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/validation/implementations/ReservationValidator.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd02/validation/implementations/ReservationValidator.java
@@ -1,14 +1,14 @@
 package pl.lodz.p.it.ssbd2022.ssbd02.validation.implementations;
 
 import pl.lodz.p.it.ssbd2022.ssbd02.mor.dto.CreateReservationDto;
-import pl.lodz.p.it.ssbd2022.ssbd02.validation.constraint.Availability;
+import pl.lodz.p.it.ssbd2022.ssbd02.validation.constraint.Reservation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class ReservationValidator implements ConstraintValidator<Availability, CreateReservationDto> {
+public class ReservationValidator implements ConstraintValidator<Reservation, CreateReservationDto> {
     @Override
-    public void initialize(Availability availability) {
+    public void initialize(Reservation reservation) {
 
     }
 
