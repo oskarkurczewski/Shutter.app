74667982c1d6fd46949c708a6f330f949032851d
Oskar Kurczewski <229935@edu.p.lodz.pl>
2022-06-18 18:38:42 +0200

    dodano prostą stronę główną

--- src/main/frontend/src/pages/home/homePage.tsx
+++ src/main/frontend/src/pages/home/homePage.tsx
@@ -1,24 +1,69 @@
 import React from "react";
+import styles from "./homePage.module.scss";
 import { Button } from "components/shared";
-import { useAppDispatch } from "redux/hooks";
 import { useAppSelector } from "redux/hooks";
 import { logout } from "redux/slices/authSlice";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { AccessLevel } from "types";
 
 export const HomePage = () => {
    const { token } = useAppSelector((state) => state.auth);
-   const dispatch = useAppDispatch();
+   const { t } = useTranslation();
+   const { accessLevel, username } = useAppSelector((state) => state.auth);
+   const buttons = {
+      GUEST: [
+         { url: "/login", label: "main_page.sign_in" },
+         { url: "/register", label: "main_page.sign_up" },
+      ],
+      CLIENT: [
+         { url: "/photographers", label: "main_page.photographers" },
+         { url: "/settings", label: "main_page.settings" },
+      ],
+      PHOTOGRAPHER: [
+         { url: "/settings", label: "main_page.settings" },
+         { url: "/profile/gallery", label: "main_page.gallery" },
+         { url: `/profile/${username}`, label: "main_page.photographer_profile" },
+      ],
+      ADMINISTRATOR: [
+         { url: "/settings", label: "main_page.settings" },
+         { url: "/users", label: "main_page.users" },
+         { url: "/photographers", label: "main_page.photographers" },
+      ],
+      MODERATOR: [
+         { url: "/users", label: "main_page.users" },
+         { url: "/photographers", label: "main_page.photographers" },
+         { url: "/settings", label: "main_page.settings" },
+      ],
+   };
+
+   const getButtons = (accessLevel) => {
+      return (
+         <>
+            {buttons[accessLevel].map((button, index) => (
+               <Link key={index} to={button.url}>
+                  <Button onClick={null}>{t(button.label)}</Button>
+               </Link>
+            ))}
+         </>
+      );
+   };
 
    return (
-      <div>
-         {token && (
-            <Button
-               onClick={() => {
-                  dispatch(logout());
-               }}
-            >
-               Logout
-            </Button>
+      <div className={styles.home_page}>
+         <img src="/icons/logo.svg" alt="shutter logo" className={styles.app_logo} />
+         <h1 className={styles.home_page_title}>SHUTTER.APP</h1>
+         {accessLevel === AccessLevel.GUEST && (
+            <p className="category-title">{t("main_page.description")}</p>
          )}
+         {accessLevel !== AccessLevel.GUEST && (
+            <p className="category-title">
+               {t("main_page.welcome")},{" "}
+               <span className={styles.username}>{username}</span>!
+            </p>
+         )}
+
+         <div className={styles.buttons_container}>{getButtons(accessLevel)}</div>
       </div>
    );
 };
