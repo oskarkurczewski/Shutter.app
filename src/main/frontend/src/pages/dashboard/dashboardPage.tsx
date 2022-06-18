70dbec4024b5f5d0f5124b03bb99fb1d7ff91a93
mdzieciuchowicz <229874@edu.p.lodz.pl>
2022-06-18 12:41:18 +0200

    Dodano input pozwalający na wybranie czasu

--- src/main/frontend/src/pages/dashboard/dashboardPage.tsx
+++ src/main/frontend/src/pages/dashboard/dashboardPage.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import styles from "./dashboardPage.module.scss";
-import { Button, DragAndDrop, ExpandableCard } from "components/shared";
+import { Button, Card, TimeInput } from "components/shared";
 import { useAppDispatch, useAppSelector } from "redux/hooks";
 import { logout } from "redux/slices/authSlice";
-import { Calendar } from "components/shared/calendar";
-import { FaPlusCircle } from "react-icons/fa";
 
 export const DashboardPage = () => {
    const dispatch = useAppDispatch();
    const { token } = useAppSelector((state) => state.auth);
-   const [isOpen, setIsOpen] = useState(false);
+   const [time, setTime] = useState<string>(null);
 
    return (
       <section className={styles.dashboard_page_wrapper}>
@@ -22,14 +20,9 @@ export const DashboardPage = () => {
                Logout
             </Button>
          )}
-         <ExpandableCard isOpen={isOpen} setIsOpen={(open) => setIsOpen(open)}>
-            <Calendar />
-         </ExpandableCard>
-         <DragAndDrop
-            icon={<FaPlusCircle />}
-            label="Dodaj nowe zdjęcie"
-            onCapture={(file) => console.log(file)}
-         />
+         <Card>
+            <TimeInput value={time} onChange={(value) => setTime(value)} />
+         </Card>
       </section>
    );
 };
