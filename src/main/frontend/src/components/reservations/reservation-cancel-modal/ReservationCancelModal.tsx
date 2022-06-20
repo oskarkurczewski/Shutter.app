cb6ea57129bdac6c468c68f6e2d8b80d3c10e00b
mdzieciuchowicz <229874@edu.p.lodz.pl>
2022-06-20 13:39:12 +0200

    Poprawa formatowania

--- src/main/frontend/src/components/reservations/reservation-cancel-modal/ReservationCancelModal.tsx
+++ src/main/frontend/src/components/reservations/reservation-cancel-modal/ReservationCancelModal.tsx
@@ -7,57 +7,56 @@ import { Toast } from "types";
 import { useCancelReservationMutation } from "redux/service/usersManagementService";
 
 interface Props {
-    isOpen: boolean;
-    setIsOpen: (value: boolean) => void;
-    reservationId: number;
-    onSubmit: () => void;
+   isOpen: boolean;
+   setIsOpen: (value: boolean) => void;
+   reservationId: number;
+   onSubmit: () => void;
 }
 
 export const ReservationCancelModal: React.FC<Props> = ({
-                                                            isOpen,
-                                                            setIsOpen,
-                                                            reservationId,
-                                                            onSubmit,
-                                                        }) => {
-    const { t } = useTranslation();
-    const dispatch = useAppDispatch();
+   isOpen,
+   setIsOpen,
+   reservationId,
+   onSubmit,
+}) => {
+   const { t } = useTranslation();
+   const dispatch = useAppDispatch();
 
-    const [cancelReservationMutation, cancelReservationMutationState] =
-        useCancelReservationMutation();
+   const [cancelReservationMutation, cancelReservationMutationState] =
+      useCancelReservationMutation();
 
-    const successToast: Toast = {
-        type: ToastTypes.SUCCESS,
-        text: t("user_reservations_page.cancel_modal.success"),
-    };
+   const successToast: Toast = {
+      type: ToastTypes.SUCCESS,
+      text: t("user_reservations_page.cancel_modal.success"),
+   };
 
-    const errorToast: Toast = {
-        type: ToastTypes.ERROR,
-        text: t("user_reservations_page.cancel_modal.error"),
-    };
+   const errorToast: Toast = {
+      type: ToastTypes.ERROR,
+      text: t("user_reservations_page.cancel_modal.error"),
+   };
 
-    useEffect(() => {
-        if (cancelReservationMutationState.isSuccess) {
-            dispatch(push(successToast));
-            onSubmit();
-        }
-        if (cancelReservationMutationState.isError) {
-            dispatch(push(errorToast));
-        }
-    }, [cancelReservationMutationState]);
+   useEffect(() => {
+      if (cancelReservationMutationState.isSuccess) {
+         dispatch(push(successToast));
+         onSubmit();
+      }
+      if (cancelReservationMutationState.isError) {
+         dispatch(push(errorToast));
+      }
+   }, [cancelReservationMutationState]);
 
-    return (
-        <Modal
-            title={t("user_reservations_page.cancel_modal.title")}
-            type="confirm"
-            isOpen={isOpen}
-            onCancel={() => setIsOpen(false)}
-            onSubmit={() => {
-                cancelReservationMutation(reservationId);
-                setIsOpen(false);
-            }}
-            submitText={t("user_reservations_page.cancel_modal.submit")}
-        >
-            <p>{t("user_reservations_page.cancel_modal.description")}</p>
-        </Modal>
-    );
+   return (
+      <Modal
+         title={t("user_reservations_page.cancel_modal.title")}
+         type="confirm"
+         isOpen={isOpen}
+         onCancel={() => setIsOpen(false)}
+         onSubmit={() => {
+            cancelReservationMutation(reservationId);
+         }}
+         submitText={t("user_reservations_page.cancel_modal.submit")}
+      >
+         <p>{t("user_reservations_page.cancel_modal.description")}</p>
+      </Modal>
+   );
 };
